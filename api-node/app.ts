6def5967b36b22bc2d2497a7a4f62448cb602dec
dsa069 <dsa069@inlumine.ual.es>
2026-05-04 17:07:49 +0000

    chore/node: traduciondo clases de js a ts

--- api-node/app.ts
+++ api-node/app.ts
@@ -1,13 +1,13 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import createError from "http-errors";
+import express, { Application, Request, Response, NextFunction } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-var indexRouter = require("./dashboard_server/routes/index");
-var usersRouter = require("./dashboard_server/routes/users");
+import indexRouter from "./dashboard_server/routes/index";
+import usersRouter from "./dashboard_server/routes/users";
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "dashboard_server/views"));
@@ -23,12 +23,12 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -38,4 +38,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+export default app;
--- api-node/dashboard_server/controllers/placeholder.md
+++
@@ -1,1 +0,0 @@
-Placeholder de lso contorladores
