abdba1c74ed07b175179e460749853e664267106
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-06 20:03:56 +0000

    chore/spring: Setup config server and Adding configuration files for microservices

--- api-spring/eureka.client.manager/src/main/java/draftkings/eureka/client/manager/client/PlayerClient.java
+++ api-spring/eureka.client.manager/src/main/java/draftkings/eureka/client/manager/client/PlayerClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("Player-ms")
+@FeignClient("Player_ms")
 public interface PlayerClient {
 
     @GetMapping("/check")
