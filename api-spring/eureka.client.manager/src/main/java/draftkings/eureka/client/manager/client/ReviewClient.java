500f9307f2f2d137016d89d8aa25230c16952e50
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-06 21:19:12 +0000

    fix/spring: Arreglando configuracion configserver, cambaindo los nombres de ms a nombres validos y añadiendo atajos produccion

--- api-spring/eureka.client.manager/src/main/java/draftkings/eureka/client/manager/client/ReviewClient.java
+++ api-spring/eureka.client.manager/src/main/java/draftkings/eureka/client/manager/client/ReviewClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("Review_ms")
+@FeignClient("reviewMS")
 public interface ReviewClient {
 
     @GetMapping("/check")
