503226e55f4c2042c89862f22259b668a7c106fc
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-07 15:03:54 +0200

    refactor/Ionic: Update Ionic CI workflow and environment files to upload to the remote

--- client-ionic/src/environments/environment.ts
+++ client-ionic/src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//ALMACENAR SECRETOS EN EL SISTEMA DE SECRETOS DE GITHUB
 export const environment = {
   production: false
 };
