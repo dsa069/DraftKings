024a78a2d0d9b4b2569e28a8125d901afece607d
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-09 00:21:03 +0200

    fix/corba: remove unused 'man' method from BufferServer class

--- corba-services/DK_News_Manager/src/Server/BufferServer.java
+++ corba-services/DK_News_Manager/src/Server/BufferServer.java
@@ -29,31 +29,10 @@ public class BufferServer {
            synchronized(sync){
                  sync.wait();
            }
-       } catch(Exception e) {
-                   System.err.println("ERROR: " + e);
-                   e.printStackTrace(System.out);
-        }
-    }
-     public static void man() {
-       try {
-    	   
-            ORB orb = ORB.init(args, null); // Crea e inicializa el ORB
-            // Crea un hilo (servant) y lo registra en el ORB
-            BufferImpl bufferRef = new BufferImpl();
-            bufferRef.setORB(orb);
-            orb.connect(bufferRef);
-            // Obtiene una referencia del objeto
-            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-            NamingContext ncRef = NamingContextHelper.narrow(objRef);
-            // Activa la referencia
-            NameComponent nc = new NameComponent("Buffer", "");
-            NameComponent path[] = {nc};
-            ncRef.rebind(path, bufferRef);
-            System.out.println("Servidor Buffer preparado y esperando ...");
-           // Espera indefinidamente atendiendo las peticiones de los clientes
-           java.lang.Object sync = new java.lang.Object();
-           synchronized(sync){
-                 sync.wait();
+           if(true) {
+        	   int x = 1/0; // para probar el catch
+           }else {
+        	   System.out.println("Servidor Buffer finalizado.");
            }
        } catch(Exception e) {
                    System.err.println("ERROR: " + e);
