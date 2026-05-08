9f39257fd547e20df81dbb851d18e59b0a0f45cb
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-09 00:18:21 +0200

    test/corba: implement BufferServer functionality and add tests for BufferImpl and BufferServer

--- corba-services/DK_News_Manager/src/Server/BufferServer.java
+++ corba-services/DK_News_Manager/src/Server/BufferServer.java
@@ -34,4 +34,30 @@ public class BufferServer {
                    e.printStackTrace(System.out);
         }
     }
+     public static void man() {
+       try {
+    	   
+            ORB orb = ORB.init(args, null); // Crea e inicializa el ORB
+            // Crea un hilo (servant) y lo registra en el ORB
+            BufferImpl bufferRef = new BufferImpl();
+            bufferRef.setORB(orb);
+            orb.connect(bufferRef);
+            // Obtiene una referencia del objeto
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            NamingContext ncRef = NamingContextHelper.narrow(objRef);
+            // Activa la referencia
+            NameComponent nc = new NameComponent("Buffer", "");
+            NameComponent path[] = {nc};
+            ncRef.rebind(path, bufferRef);
+            System.out.println("Servidor Buffer preparado y esperando ...");
+           // Espera indefinidamente atendiendo las peticiones de los clientes
+           java.lang.Object sync = new java.lang.Object();
+           synchronized(sync){
+                 sync.wait();
+           }
+       } catch(Exception e) {
+                   System.err.println("ERROR: " + e);
+                   e.printStackTrace(System.out);
+        }
+    }
 }
