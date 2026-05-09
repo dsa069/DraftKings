df2a04037025c885b86d2358a7d0bf2efb992612
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-09 23:14:22 +0200

    fix/corba: enhance Terraform deployment with dynamic credentials handling and improved variable management

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,9 @@ variable "dns_managed_zone" {
   description = "Cloud DNS managed zone name (not the FQDN)"
   type        = string
 }
+
+variable "credentials_file" {
+  description = "Ruta al archivo JSON de credenciales de GCP"
+  type        = string
+  default     = "./credentials/gc-dk.json" # Tu ruta local
+}
