3e9328ce5ecc6049726fdc2d5866548855c2a2e4
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-09 22:15:33 +0000

    fix/corba: update Docker installation command in initialization script

--- terraform/mynetwork.tf
+++ terraform/mynetwork.tf
@@ -50,7 +50,7 @@ resource "null_resource" "provision-deploy-vm" {
     ## Script inicialización corba-deploy-vm
     inline = [
       "sudo apt-get update -y",
-      "sudo apt-get install -y docker.io", # Instala el motor de Docker
+      "sudo apt install -y docker.io", # Instala el motor de Docker
       "sudo systemctl start docker",       # Levanta el servicio
       "sudo systemctl enable docker",      # Asegura que arranque tras reinicios
       "sudo usermod -aG docker ubuntu"     # Permite al usuario usar docker"
