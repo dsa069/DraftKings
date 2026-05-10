5d5cc41c00cfe964a4d6a6e7d21cfdfeb14127e3
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-10 14:30:29 +0000

    ci/node: update coverage reporting in Jest and CI workflow

--- api-node/jest.config.js
+++ api-node/jest.config.js
@@ -3,7 +3,9 @@ const { createDefaultPreset } = require("ts-jest");
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-export default {
+module.exports = {
+  // Cambiado export default por module.exports
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
