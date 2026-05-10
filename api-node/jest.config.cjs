d58d4af7cc47398ba3adc8f82116e5d6f3e8f75f
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-10 14:17:31 +0000

    ci/node: add ESLint configuration, update Jest config, and modify package.json scripts for TypeScript support

--- api-node/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>"],
-  testMatch: ["**/test/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
-  moduleFileExtensions: ["ts", "js", "json", "node"],
-};
