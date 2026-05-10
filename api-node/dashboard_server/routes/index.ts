a55e83e47a3ab7a84fd161c23764cb968523886d
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-10 13:29:59 +0000

    chore/node: add dotenv support and update environment handling in app for prod and dev

--- api-node/dashboard_server/routes/index.ts
+++ api-node/dashboard_server/routes/index.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { Request, Response } from "express";
 import { getHome } from "../controllers/dashboard";
 
 const router = Router();
 
-// GET home page
-router.get("/", getHome);
+router.get("/", (req: Request, res: Response) => {
+  // Cogemos la variable de entorno
+  const mensajeParaFrontend = process.env.APP_MESSAGE;
+
+  // Se la pasamos a la vista Pug
+  res.render("index", {
+    title: "Mi App",
+    welcomeMessage: mensajeParaFrontend,
+  });
+});
 
 export default router;
