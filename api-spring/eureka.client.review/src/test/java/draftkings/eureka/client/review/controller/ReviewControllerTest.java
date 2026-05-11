3f81d629561fa7d737a37bbc70becdd89e4a277c
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-11 20:37:42 +0000

    ci/spring: remove deployment jobs from CI workflow and add H2 dependency for testing

--- api-spring/eureka.client.review/src/test/java/draftkings/eureka/client/review/controller/ReviewControllerTest.java
+++ api-spring/eureka.client.review/src/test/java/draftkings/eureka/client/review/controller/ReviewControllerTest.java
@@ -3,10 +3,12 @@ package draftkings.eureka.client.review.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ReviewControllerTest {
 
