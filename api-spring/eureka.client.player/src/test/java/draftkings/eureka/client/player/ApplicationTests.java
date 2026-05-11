d13798556366c15c34a2ad6d62e5b41cf468d086
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-11 20:02:23 +0000

    ci/spring: set active profile for tests for ci compatibility player client

--- api-spring/eureka.client.player/src/test/java/draftkings/eureka/client/player/ApplicationTests.java
+++ api-spring/eureka.client.player/src/test/java/draftkings/eureka/client/player/ApplicationTests.java
@@ -2,7 +2,9 @@ package draftkings.eureka.client.player;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApplicationTests {
 
