fef572c004f8bc86b8b938e9485e96ba48be408a
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-11 20:37:42 +0000

    ci/spring: remove deployment jobs from CI workflow and add H2 dependency for testing

--- api-spring/gateway/src/test/java/draftkings/gateway/ApplicationTests.java
+++ api-spring/gateway/src/test/java/draftkings/gateway/ApplicationTests.java
@@ -2,9 +2,11 @@ package draftkings.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApplicationTests {
 
