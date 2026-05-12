77c1dc85be85abc6d2f0962ff12cb387b1ab1ef6
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-12 13:20:32 +0000

    fix/spring: enable discovery client in application configuration

--- api-spring/config.server/src/main/java/draftkings/config/server/Application.java
+++ api-spring/config.server/src/main/java/draftkings/config/server/Application.java
@@ -3,9 +3,11 @@ package draftkings.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
