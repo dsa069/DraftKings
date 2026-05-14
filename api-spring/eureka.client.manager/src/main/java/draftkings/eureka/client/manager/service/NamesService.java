08cf18e4d87aa294b11e887e44434fd1911039d4
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-14 19:39:06 +0000

    Refactor/spring: Remove eureka.client.manager module and related configurations
    - Deleted eureka.client.manager module including all source files, Dockerfiles, and configuration files.
    - Updated application YAML configurations for playerMS and removed unnecessary eureka.client.manager references.
    - Adjusted setup aliases to remove manager-related commands.

--- api-spring/eureka.client.manager/src/main/java/draftkings/eureka/client/manager/service/NamesService.java
+++
@@ -1,7 +0,0 @@
-package draftkings.eureka.client.manager.service;
-
-public interface NamesService {
-    public String getPlayerName(String playerName);
-
-    public String getReviewName(String reviewName);
-}
