d8a0f4250d33d029682dec8e30fceabd24b17b6c
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-14 16:50:04 +0000

    refactor/ionic: Simplify factory functions by removing HttpClient dependency and using inject for service instantiation

--- client-ionic/src/app/core/services/implementations/player.service.ts
+++ client-ionic/src/app/core/services/implementations/player.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Player } from '../../models/player.model';
+import { Injectable, inject } from '@angular/core';
 
+@Injectable()
 export abstract class PlayerService {
   protected abstract apiUrl: string; // Cada hijo dirá su URL
 
-  constructor(protected http: HttpClient) {}
+  protected http = inject(HttpClient);
 
   getPlayer(): Observable<Player[]> {
     return this.http.get<Player[]>(`${this.apiUrl}/players`);
--- client-ionic/src/app/core/services/implementations/review.service.ts
+++ client-ionic/src/app/core/services/implementations/review.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Review } from '../../models/review.model';
+import { Injectable, inject } from '@angular/core';
 
+@Injectable()
 export abstract class ReviewService {
   protected abstract apiUrl: string; // Cada hijo dirá su URL
 
-  constructor(protected http: HttpClient) {}
+  protected http = inject(HttpClient);
 
   getReview(): Observable<Review[]> {
     return this.http.get<Review[]>(`${this.apiUrl}/reviews`);
