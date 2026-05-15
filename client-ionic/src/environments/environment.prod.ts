54a0ede0793188a09c25b409430b7e688ce3371b
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-15 12:50:14 +0000

    feat/ionic: setup Firebase variables configuration and add environment setup script

--- client-ionic/src/environments/environment.prod.ts
+++ client-ionic/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
-//ALMACENAR SECRETOS EN EL SISTEMA DE SECRETOS DE GITHUB
 export const environment = {
   production: true,
-  firebaseConfig: {},
+  firebaseConfig: {
+    apiKey: 'AIzaSyAsExampleKey_1234567',
+    authDomain: 'tu-proyecto-prod.firebaseapp.com',
+    projectId: 'tu-proyecto-prod',
+    storageBucket: 'tu-proyecto-prod.appspot.com',
+    messagingSenderId: '1234567890',
+    appId: '1:1234567890:web:abc123def456gh',
+  },
 };
