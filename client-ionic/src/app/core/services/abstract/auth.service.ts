cc30f1127ec242379f16ba7fd4d1379bffe3081c
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-17 20:39:21 +0000

    feat/ionic: Cerrar sesion

--- client-ionic/src/app/core/services/abstract/auth.service.ts
+++ client-ionic/src/app/core/services/abstract/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user.model';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, Signal } from '@angular/core';
 
 @Injectable()
 export abstract class AuthService {
   protected abstract apiUrl: string; // Cada hijo dirá su URL
   protected http = inject(HttpClient);
+  public abstract readonly isAuthenticated: Signal<boolean>;
 
   abstract login(email: string, pass: string): Promise<any>;
   abstract register(email: string, pass: string, extraData?: any): Promise<any>;
