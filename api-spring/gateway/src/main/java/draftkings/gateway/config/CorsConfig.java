7f7f709a6fcb211c6ee1e4d5102fb7ef25199dbb
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-20 16:06:02 +0000

    feat/spring: update CORS configuration across gateway YAML files and CorsConfig class

--- api-spring/gateway/src/main/java/draftkings/gateway/config/CorsConfig.java
+++ api-spring/gateway/src/main/java/draftkings/gateway/config/CorsConfig.java
@@ -1,23 +1,30 @@
 package draftkings.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
+    // Inyectamos la lista definida en el application.yml
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8100"));
-        corsConfig.setMaxAge(3600L); // Opcional: Cachea el preflight 1 hora
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+
+        // Ahora usamos la lista dinámica
+        corsConfig.setAllowedOrigins(allowedOrigins);
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(List.of("*"));
         corsConfig.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
