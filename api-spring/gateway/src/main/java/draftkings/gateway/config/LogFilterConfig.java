7f49187da108277359c804788530b6510fcff9af
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-21 12:39:16 +0200

    docs/shared: README auth y android apk

--- api-spring/gateway/src/main/java/draftkings/gateway/config/LogFilterConfig.java
+++ api-spring/gateway/src/main/java/draftkings/gateway/config/LogFilterConfig.java
@@ -1,5 +1,5 @@
 package draftkings.gateway.config;
-
+/*
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -31,4 +31,4 @@ public class LogFilterConfig {
             }));
         };
     }
-}
+}*/
