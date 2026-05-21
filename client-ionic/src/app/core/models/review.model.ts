80dae85b01a218a494bf61f5e6a379b217ede474
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-21 14:59:01 +0200

    chore/node: models add missing newlines at the end of files for Player, Review, and User interfaces

--- client-ionic/src/app/core/models/review.model.ts
+++ client-ionic/src/app/core/models/review.model.ts
@@ -4,12 +4,12 @@ import { Player } from './player.model';
 export interface Review {
   id?: number;
   userId: number | string; // ID o UID de la base de datos interna / Firebase
-  user?: User;             // Relación opcional cargada por populate/join
+  user?: User; // Relación opcional cargada por populate/join
   playerId: number;
-  player?: Player;         // Relación opcional cargada por populate/join
+  player?: Player; // Relación opcional cargada por populate/join
   author: string;
   text: string;
-  rating: number;          // Validación en frontend: entre 0 y 5
+  rating: number; // Validación en frontend: entre 0 y 5
   latitude: number;
   longitude: number;
   created_at?: string | Date;
