dcfe5b526f25601d7d2c2a5563a6eb64faa99ccb
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-22 16:53:45 +0000

    feat/ionic: implement new player form with detailed input fields and styling

--- client-ionic/src/app/features/new-player/new-player.page.ts
+++ client-ionic/src/app/features/new-player/new-player.page.ts
@@ -5,8 +5,26 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+  IonItem,
+  IonAvatar,
+  IonIcon,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonImg,
+  IonFooter,
 } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { addIcons } from 'ionicons';
+import { person, camera, location } from 'ionicons/icons';
 
 @Component({
   selector: 'app-new-player',
@@ -18,12 +36,31 @@ import { HeaderComponent } from '../../shared/components/header/header.component
     IonHeader,
     IonTitle,
     IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonLabel,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+    IonText,
+    IonItem,
+    IonAvatar,
+    IonIcon,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonImg,
+    IonFooter,
     HeaderComponent,
     FormsModule,
   ],
 })
 export class NewPlayerPage implements OnInit {
-  constructor() {}
+  constructor() {
+    // Registramos los íconos globalmente para este standalone component
+    addIcons({ person, camera, location });
+  }
 
   ngOnInit() {
     // Temporal para que el linter no de error
