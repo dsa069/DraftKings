3215dc1617b53c62f382b44d6f68b4bebd20ea0a
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-22 23:25:37 +0000

    feat/ionic: implement my team page layout with tactical pitch and player details

--- client-ionic/src/app/features/tabs/my-team/my-team.page.ts
+++ client-ionic/src/app/features/tabs/my-team/my-team.page.ts
@@ -4,15 +4,61 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent,
+  IonText,
+  IonButton,
+  IonIcon,
+  IonBadge,
+  IonChip,
+  IonLabel,
+  IonAvatar,
+  IonImg,
 } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { addIcons } from 'ionicons';
+import {
+  optionsOutline,
+  sparkles,
+  bulb,
+  gitNetworkOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-my-team',
   templateUrl: 'my-team.page.html',
   styleUrls: ['my-team.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardContent,
+    IonText,
+    IonButton,
+    IonIcon,
+    IonBadge,
+    IonChip,
+    IonLabel,
+    IonAvatar,
+    IonImg,
+    HeaderComponent,
+  ],
 })
 export class MyTeamPage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      optionsOutline,
+      sparkles,
+      bulb,
+      gitNetworkOutline,
+    });
+  }
 }
