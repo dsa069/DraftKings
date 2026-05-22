45af77161fc3980b643677764d0b6d532c609fb0
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-22 11:16:14 +0000

    feat/ionic: view players page layout and functionality with search and filter options

--- client-ionic/src/app/features/tabs/players/players.page.ts
+++ client-ionic/src/app/features/tabs/players/players.page.ts
@@ -4,15 +4,65 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonIcon,
+  IonInput,
+  IonButton,
+  IonList,
+  IonAvatar,
+  IonImg,
+  IonLabel,
+  IonText,
+  IonFab,
 } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { addIcons } from 'ionicons';
+import {
+  searchOutline,
+  optionsOutline,
+  star,
+  starHalf,
+  add,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-players',
   templateUrl: 'players.page.html',
   styleUrls: ['players.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonItem,
+    IonIcon,
+    IonInput,
+    IonButton,
+    IonList,
+    IonAvatar,
+    IonImg,
+    IonLabel,
+    IonText,
+    IonFab,
+    HeaderComponent,
+  ],
 })
 export class PlayersPage {
-  constructor() {}
+  constructor() {
+    // Registramos los iconos equivalentes a los que usabas de Material Symbols
+    addIcons({
+      searchOutline,
+      optionsOutline,
+      star,
+      starHalf,
+      add,
+    });
+  }
 }
