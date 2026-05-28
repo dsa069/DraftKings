a305ba30d75fce0a954446d2a82da41e15e1d81d
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-28 15:56:56 +0000

    test/ionic: add TeamService provider to various components for team management functionality

--- client-ionic/src/app/features/create-players-news/create-players-news.page.cy.ts
+++ client-ionic/src/app/features/create-players-news/create-players-news.page.cy.ts
@@ -6,11 +6,20 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { TeamService } from '../../core/services/abstract/team.service';
 
 describe('CreatePlayersNews Component', () => {
   it('should render title', () => {
     cy.mount(CreatePlayersNewsPage, {
       imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+      providers: [
+        {
+          provide: TeamService,
+          useValue: {
+            clearTeam: async () => undefined,
+          },
+        },
+      ],
     });
 
     cy.get('ion-title').should('exist');
--- client-ionic/src/app/shared/components/header/header.component.cy.ts
+++ client-ionic/src/app/shared/components/header/header.component.cy.ts
@@ -6,11 +6,20 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { TeamService } from '../../../core/services/abstract/team.service';
 
 describe('HeaderComponent', () => {
   it('should render title', () => {
     cy.mount(HeaderComponent, {
       imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+      providers: [
+        {
+          provide: TeamService,
+          useValue: {
+            clearTeam: async () => undefined,
+          },
+        },
+      ],
     });
 
     cy.get('ion-title').should('exist');
