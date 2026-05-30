c2e2e26ec22d6312a643188058ed94e0e8cc9977
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-30 15:33:03 +0000

    fix/node: switch to ES module syntax and improve linting command

--- api-node/jest.config.js
+++ api-node/jest.config.js
@@ -1,13 +1,17 @@
-const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["<rootDir>/test/setup-env.ts"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/", "<rootDir>/coverage/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/coverage/",
+  ],
   transform: {
     ...tsJestTransformCfg,
   },
