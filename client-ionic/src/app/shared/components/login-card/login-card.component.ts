e6f91a60eb8966eac2ca4717b658b105f143cc0d
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-05-30 17:56:23 +0000

    refactor/ionic: remove backend selection from login and sign-up pages, integrate backend toggle in switch-back page

--- client-ionic/src/app/shared/components/login-card/login-card.component.ts
+++ client-ionic/src/app/shared/components/login-card/login-card.component.ts
@@ -13,7 +13,6 @@ import {
   IonFooter,
   IonText,
 } from '@ionic/angular/standalone';
-import { BackendToggleComponent } from '../backend-toggle/backend-toggle.component'; // Ajusta la ruta si es necesario
 import { addIcons } from 'ionicons';
 import { arrowForwardOutline } from 'ionicons/icons';
 
@@ -22,28 +21,16 @@ import { arrowForwardOutline } from 'ionicons/icons';
   templateUrl: './login-card.component.html',
   styleUrls: ['./login-card.component.scss'],
   standalone: true,
-  imports: [
-    IonCard,
-    IonCardContent,
-    IonButton,
-    IonIcon,
-    IonFooter,
-    IonText,
-    BackendToggleComponent,
-  ],
+  imports: [IonCard, IonCardContent, IonButton, IonIcon, IonFooter, IonText],
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginCardComponent {
   @Input() footerText: string = '';
   @Input() footerActionText: string = '';
-
-  // Backend selection abstraction
-  @Input() selectedBackend: 'node' | 'springboot' = 'node';
   @Input() submitText: string = 'Confirm';
-  @Input() formId: string = ''; // ID del formulario en el padre
+  @Input() formId: string = '';
 
   @Output() footerActionClick = new EventEmitter<void>();
-  @Output() backendChange = new EventEmitter<'node' | 'springboot'>();
 
   constructor() {
     addIcons({ arrowForwardOutline });
@@ -51,9 +38,5 @@ export class LoginCardComponent {
 
   onFooterAction() {
     this.footerActionClick.emit();
-  }
-
-  onBackendChange(val: 'node' | 'springboot') {
-    this.backendChange.emit(val);
   }
 }
